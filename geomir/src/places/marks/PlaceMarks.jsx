e22131e66ec4ea5a01cddad16e24c1c6ed7bbb14
Iñaki Galan Mata <inakigalan17@gmail.com>
2023-03-13 16:35:37 +0100

    fix

--- geomir/src/places/marks/PlaceMarks.jsx
+++ geomir/src/places/marks/PlaceMarks.jsx
@@ -1,6 +1,6 @@
 import React, { useEffect, useReducer } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
-import PlaceMark from './PlaceMark';
+import PlaceMark from './placeMark';
 
 const PlaceMarks = () => {
 
